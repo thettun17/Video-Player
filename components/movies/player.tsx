4e7da6b94f796f0116ad131395a540ffeb8ef90e
Thet Tun <thettun.loard17@gmail.com>
2024-12-28 16:51:59 +0700

    without using hls js

--- components/movies/player.tsx
+++ components/movies/player.tsx
@@ -10,23 +10,23 @@ interface Player {
 // const Player = ({ url }: Player) => {
 const Player = () => {
     const videoUrl = 'https://movie.darksidemm.com/file/black-blaze/ssstik.io_1732907726778.mp4';   
-    const playerRef = useRef(null);
-     useEffect(() => { 
-        if (playerRef.current) { 
-            const video = playerRef.current; 
-            if (Hls.isSupported() && video) { 
-                const hls = new Hls(); 
-                hls.loadSource(videoUrl); 
-                hls.attachMedia(video); 
-            } 
-        } 
-    }, [videoUrl]);
+    // const playerRef = useRef(null);
+    //  useEffect(() => { 
+    //     if (playerRef.current) { 
+    //         const video = playerRef.current; 
+    //         if (Hls.isSupported() && video) { 
+    //             const hls = new Hls(); 
+    //             hls.loadSource(videoUrl); 
+    //             hls.attachMedia(video); 
+    //         } 
+    //     } 
+    // }, [videoUrl]);
     return (
         <div className="relative h-[80%] m-4 bg-red-400">
             <div className="absolute top-0 left-0 bottom-0 right-0 bg-gray-900">
                 <ReactPlayer
                     className='react-player'
-                    ref={playerRef}
+                    // ref={playerRef}
                     url={videoUrl}
                     controls={true}
                     width='100%'
@@ -37,11 +37,7 @@ const Player = () => {
                         file: {
                             attributes: {
                                 controlsList: 'nodownload'
-                            },
-                            hlsOptions: {
-                                startLevel: 3
-                            },
-                            hlsVersion: '0.13.1',
+                            }
                         }
                     }}
                 />
